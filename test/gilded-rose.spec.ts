ef7f8dbd77fec825e2766d142994586702e37f3b
Leo Ng <leo.ng@softwire.com>
2022-08-18 13:36:01 +0100

    leo try

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -9,4 +9,13 @@ describe('Gilded Rose', function () {
         expect(items[0].name).to.equal('fixme');
     });
 
+    it.only('experiment to see what the expected output is', function(){
+        const gildedRose = new GildedRose([ new Item('foo', 0, 0), new Item('item2', 0, 0)]);
+        const item1 = gildedRose.updateQuality();
+        
+        console.log(item3);
+
+        expect(item3[0].name).to.equal('foo');
+        expect(item3[0].sellIn).to.equal(-3);
+    });
 });
